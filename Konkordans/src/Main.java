2a427720222b9875cb8263d9d26e75fb3ee31b09
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2015-09-15 16:43:05 +0200

    forgot to add Main to commit before last

--- Konkordans/src/Main.java
+++ Konkordans/src/Main.java
@@ -7,40 +7,9 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Constructor c = new Constructor();
+		c.construct();
 
-	}
-	
-	public void construct() {
-		//tell the position of the word
-		try {
-			File index = File.createTempFile("/var/tmp/index", "tmp");
-			File pos = File.createTempFile("/var/tmp/pos", "tmp");
-			File tokens = new File("/var/tmp/ut");
-			
-			BufferedWriter indexWriter = new BufferedWriter(new FileWriter(index));
-			OutputStream posWriter = new FileOutputStream("/var/tmp/pos");
-			FileReader fr = new FileReader(tokens);
-			BufferedReader br = new BufferedReader(fr);
-			
-			String line;
-			String word = null;
-			int i = 0;
-			while ((line = br.readLine()) != null) {
-				String[] stringArray = line.split(" ");
-				if (stringArray[0] != word) {
-					word = stringArray[0];
-					indexWriter.append(word + " " + i);
-				}
-				posWriter.write(stringArray[1].getBytes("ISO-8859-1"));
-				i++;
-			}
-			indexWriter.close();
-			posWriter.close();
-			br.close();
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
 	}
 
 	/**

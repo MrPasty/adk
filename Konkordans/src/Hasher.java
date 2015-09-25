091a3564e9048539ba78d512b1b14b6edeafdd5a
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2015-09-25 15:22:59 +0200

    make hasher actually work, maybe

--- Konkordans/src/Hasher.java
+++ Konkordans/src/Hasher.java
@@ -1,11 +1,13 @@
-
 public class Hasher {
 
+	private static String alph = " abcdefghijklmnopqrstuvwxyzäåö";
+	private static int[] m = {1, 30, 900};
+	
 	public static int hash (String s) {
 		char[] ca = s.toCharArray();
 		int res = 0;
-		for (char c : ca) {
-			res = res + c;
+		for (int i = 0; i < ca.length; i++) {
+			res += alph.indexOf(i) * m[i];
 		}
 		return res;
 	}

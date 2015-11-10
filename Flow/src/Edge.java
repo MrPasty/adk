979465807e8a7e2118276734c6f2bbb329e39b04
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2015-11-10 15:16:52 +0100

    add constructors to Edge class

--- Flow/src/Edge.java
+++ Flow/src/Edge.java
@@ -1,6 +1,7 @@
 
 public class Edge {
 	int a, b, cap, flow;
+	Edge rev;
 	
 	public Edge (int a, int b) {
 		new Edge(a, b, 1);
@@ -11,9 +12,14 @@ public class Edge {
 	}
 	
 	public Edge (int a, int b, int cap, int flow) {
+		new Edge(a, b, cap, flow, this);
+	}
+	
+	public Edge (int a, int b, int cap, int flow, Edge rev) {
 		this.a = a;
 		this.b = b;
 		this.cap = cap;
 		this.flow = flow;
+		this.rev = rev;		
 	}
 }

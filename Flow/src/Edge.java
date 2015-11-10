3a7e9911ac942b0d7ad14417fdeaad917efc0d08
Sebastian Magnusson <sebbe.magnusson@gmail.com>
2015-11-10 16:37:18 +0100

    undo uglification, use "this" instead

--- Flow/src/Edge.java
+++ Flow/src/Edge.java
@@ -4,20 +4,15 @@ public class Edge {
 	Edge rev;
 	
 	public Edge (int a, int b) {
-		new Edge(a, b, 1);
+		this(a, b, 1);
 	}
 	
 	public Edge (int a, int b, int cap) {
-		new Edge(a, b, cap, 0);
+		this(a, b, cap, 0);
 	}
 	
 	public Edge (int a, int b, int cap, int flow) {
-		this.a = a;
-		this.b = b;
-		this.cap = cap;
-		this.flow = flow;
-		cap = cap < flow ? flow : cap;
-		this.rev = this;
+		this(a, b, cap, flow, null);
 	}
 	
 	public Edge (int a, int b, int cap, int flow, Edge rev) {
